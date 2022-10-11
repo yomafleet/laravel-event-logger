a39f853619b1ba4aaeeac27c1070d1947f6008c2
aungchitminn <99715319+aungchitminn@users.noreply.github.com>
2022-10-11 09:46:55 +0630

    update test case

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array<int, string>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Yomafleet\EventLogger\ServiceProvider',
+        ];
+    }
+
+    /**
+     * Override application aliases.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array<string, string>
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'EventLogger' => 'Yomafleet\EventLogger\EventLoggerFacade',
+        ];
+    }
+}
